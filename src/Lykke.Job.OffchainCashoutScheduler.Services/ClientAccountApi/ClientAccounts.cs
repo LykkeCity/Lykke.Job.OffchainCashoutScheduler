91af9e3a6731e8927ba55d619292d67b855ce4a0
Emmanuel Nabagabo <emmanuel.nabagabo@lykke.com>
2018-05-12 01:42:33 +0500

    Removed appsettings

--- src/Lykke.Job.OffchainCashoutScheduler.Services/ClientAccountApi/ClientAccounts.cs
+++ src/Lykke.Job.OffchainCashoutScheduler.Services/ClientAccountApi/ClientAccounts.cs
@@ -51,7 +51,6 @@ namespace Lykke.Job.OffchainCashoutScheduler.Services.ClientAccountApi
 
             httpClient.BaseAddress = new Uri($"{_connectionString}/api/ClientAccountInformation/getClientById");
             httpClient.DefaultRequestHeaders.Accept.Clear();
-
         }
 
        
